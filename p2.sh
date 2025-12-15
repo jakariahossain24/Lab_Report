b4f3c933b0fd95bb5a1a3d2155b6208481710e2f
Jakaria Hossain <160001536+jakariahossain24@users.noreply.github.com>
2025-12-15 12:17:26 +0600

    Problem 2
    Lab Report Problem 2

---
+++ p2.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+read -p "Enter starting year: " start
+read -p "Enter ending year: " end
+
+coun=0
+
+echo "Leap years between $start and $end are:"
+for ((year = start; year <= end; year++))
+do
+    if (( ( year % 400 == 0 ) || ( year % 4 == 0 && year % 100 != 0 ) ));
+then
+        echo $year
+        count=$(( count + 1 ))
+    fi
+done
+
+echo "Total number of leap years: $count"
